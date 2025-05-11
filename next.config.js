d36f81fae3947a33cd2690e7ca12c481eacd7d42
chengkeshuai <chengks2008@gmail.com>
2025-05-11 20:01:50 +0800

    使用JavaScript格式

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  distDir: '.next',
+  transpilePackages: ['@tailwindcss/postcss']
+};
+
+module.exports = nextConfig; 
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: '.next',
-  transpilePackages: ['@tailwindcss/postcss']
-};
-
-export default nextConfig;
